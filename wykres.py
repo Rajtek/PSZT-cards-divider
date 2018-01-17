865d41a05a6df48efc98b19447a5c258de72c5b2
Rafal <rafalkoguciuk33@gmail.com>
2018-01-18 00:07:47 +0100

    Wyniki

--- wykres.py
+++ wykres.py
@@ -1,7 +1,38 @@
 import sys
 import matplotlib
 import pylab
+"""
+def main(argv):
 
+	plik = open("result.txt", "r")
+	liczby = []
+	srednia = []
+	a = 0
+	for val in plik.read().split():
+		if a%2 == 0:
+			liczby.append(int(val))
+		else:
+			srednia.append(float(val))
+		a += 1
+	plik.close()
+	x = range(0, len(liczby) )
+	m = max(srednia)
+
+
+	
+	pylab.plot(liczby, srednia, 'r')
+	pylab.xlabel('Liczba kart')
+	pylab.ylabel('Czas dzialania algorytmu')
+	pylab.title('Algorytm zachlanny')
+	pylab.grid(True)
+	pylab.savefig('zachlanny.png')
+
+	return
+	
+if __name__ == "__main__":
+	main(sys.argv[1:])
+
+"""
 def main(argv):
 
 	plik = open(argv[0]+"_result.txt", "r")
@@ -33,4 +64,3 @@ def main(argv):
 	
 if __name__ == "__main__":
 	main(sys.argv[1:])
-
